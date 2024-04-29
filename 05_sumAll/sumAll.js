ad656609d8d829523c231dedda77e686ef7cd193
João Pedro <jpedrocruz78@gmail.com>
2024-04-29 15:48:55 -0400

    finish exercise number 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,35 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
+    let sum = 0;
+    let biggerNumber;
+    let smallerNumber;
+    let typeOfNumberOne;
+    let typeOfNumberTwo;
+    typeOfNumberOne = typeof numberOne;
+    typeOfNumberTwo = typeof numberTwo;
+    if( typeOfNumberOne != "number" || typeOfNumberTwo != "number")
+    {
+        return "ERROR";
+    }
+    if (numberOne < 0  || numberTwo < 0) 
+    {
+        return "ERROR";
+    }   
+    if (numberOne > numberTwo)
+    {
+        biggerNumber = numberOne;
+        smallerNumber = numberTwo;
+    }else
+    {
+        biggerNumber = numberTwo;
+        smallerNumber = numberOne;
+    }
+    for(index = 0; index < biggerNumber; index++)
+    { 
 
+        sum = sum + (smallerNumber + index)
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
