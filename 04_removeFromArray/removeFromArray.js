87eb1c56cb1e66ba8f5629db033737b2c3123658
João Pedro <jpedrocruz78@gmail.com>
2024-04-29 15:10:50 -0400

    finish exercise number 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+ 
+const removeFromArray = function(array, ...elementsToRemove) {
+    const newArray = [];
+    array.forEach(element => {
+        isIncluded = elementsToRemove.includes(element);
+        if (isIncluded !== true)
+        {
+            newArray.push(element);
+        };
+    });
+    return newArray;
 
 };
+//  newArray = removeFromArray([1, 2, 3, 4], 3)
+//  console.log(newArray)
 
 // Do not edit below this line
 module.exports = removeFromArray;
