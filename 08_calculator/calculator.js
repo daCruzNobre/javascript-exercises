b215828df9838b21e9f620f41b39257aaaecedf5
João Pedro <jpedrocruz78@gmail.com>
2024-05-14 11:04:23 -0400

    finish javascript-exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,33 @@
-const add = function() {
-	
+const add = function(numberOne, numberTwo) {
+	return numberOne + numberTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numberOne, numberTwo) {
+	return numberOne - numberTwo;
+};
+const sum = function(array) {
+  return array.reduce((sumOfArray, currentItem) => sumOfArray + currentItem, 0)
 };
 
-const sum = function() {
-	
+const multiply = function(array) {
+  return array.reduce((productOfArray, currentItem) => productOfArray * currentItem, 1)
 };
 
-const multiply = function() {
-
+const power = function(numberOne, numberTwo) {
+  let power = 1;
+	for(index = 0; index < numberTwo; index ++){power = numberOne * power}
+  return power;
 };
 
-const power = function() {
-	
+const factorial = function(number) {
+  if (number === 0) return 1;
+  factorialResult = 1
+  for(index = number; index > 0; index --){
+    factorialResult *= index; 
+  }
+  return factorialResult
 };
-
-const factorial = function() {
-	
-};
-
+ 
 // Do not edit below this line
 module.exports = {
   add,
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    stringArray = string.split("");  
+    stringArray = stringArray.filter(element => element !== " ")
+    stringArray = stringArray.filter(element => element !== ",")
+    stringArray = stringArray.filter(element => element !== ".")
+    stringArray = stringArray.filter(element => element !== "!")    
+           
+    reverseArray = stringArray.toReversed();
+    reverseString = reverseArray.join("");
+    string = stringArray.join("");
+    
+    if(string === reverseString){
+        return true;
+    } else {
+        return false;
+    };  
 
 };
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
