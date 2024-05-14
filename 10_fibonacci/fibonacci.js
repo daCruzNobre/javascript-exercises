78da425b1971a86b6ee32845320d8c66c061356e
João Pedro <jpedrocruz78@gmail.com>
2024-05-14 14:09:02 -0400

    finish javascript exercises 10 and 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if(index < 0) {return "OOPS"};
+    if (index == 0 || index == 1) {
+        return Number(index);
+    }
+    return fibonacci(index-1) + fibonacci(index - 2);
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
+
